5df27c7ea72a2b3d9724cc76c976c4019400af55
Shuva <a.shuv4ev@yandex.ru>
2025-04-25 19:07:15 +0300

    Unit tests: Удалены лишние комментари и добавлены тесты на два метода

--- tests.py
+++ tests.py
@@ -4,25 +4,6 @@ import pytest
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
 class TestBooksCollector:
-    """
-    Этот пример теста не работает, так как в классе BooksCollector нет метода get_books_rating
-    Написал тест по добавлению книги ниже 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
-
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
-
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
-    """
    
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
@@ -127,3 +108,26 @@ class TestBooksCollector:
         collect.delete_book_from_favorites('Гордость и предубеждение и зомби')
         
         assert collect.get_list_of_favorites_books() == []
+        
+    def test_get_list_of_favorites_books_add_two_books_success(self):
+        collect = BooksCollector()
+        
+        collect.add_new_book('Гордость и предубеждение и зомби')
+        collect.add_book_in_favorites('Гордость и предубеждение и зомби')
+        collect.add_new_book('Кладбище домашних животных')
+        collect.add_book_in_favorites('Кладбище домашних животных')
+        
+        assert collect.get_list_of_favorites_books() == ['Гордость и предубеждение и зомби', 'Кладбище домашних животных']
+    
+    def test_get_books_with_specific_genre_one_genre_success(self):
+        collect = BooksCollector()
+        
+        collect.add_new_book('Гордость и предубеждение и зомби')
+        collect.set_book_genre('Гордость и предубеждение и зомби', 'Фантастика')
+        collect.add_new_book('Кладбище домашних животных')
+        collect.set_book_genre('Кладбище домашних животных', 'Ужасы')
+        collect.add_new_book('Задача трех тел')
+        collect.set_book_genre('Задача трех тел', 'Фантастика')
+        
+        assert collect.get_books_with_specific_genre('Фантастика') == ['Гордость и предубеждение и зомби', 'Задача трех тел']
+
